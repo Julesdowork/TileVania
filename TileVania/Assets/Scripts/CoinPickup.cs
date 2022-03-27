dff581b74e41b03c3554639a39860fc08329940d
Julian McNeill <julesdowork@gmail.com>
2022-03-27 00:36:48 -0400

    Coin Sound Effect

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinSound;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(coinSound, Camera.main.transform.position);
+
             Destroy(gameObject);
         }
     }
